0ca647ae6146d105479d13eff1c3c365ae9201fe
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-02 01:25:48 +0530

    more router

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 
-
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./user'));
+
 
 module.exports = router;
