0b2310f5de6f1b41daf3df78f7cad65aa10f917b
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-04 16:08:09 +0530

    create sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,9 @@
+const User = require('../models/user');
 module.exports.profile = function(req, res){
     return res.render('profile', {
         title: "Amar"
     });
+    
 }
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
@@ -16,7 +18,20 @@ module.exports.singIn = function(req, res){
 }
 
 module.exports.create = function(req, res){
-    //todo
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in siging up'); return;}
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in creating user in siging up'); return;}
+                return res.redirect('/users/sign-in');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
 
 module.exports.createSession = function(req, res){
--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+//use static file
+app.use(express.static('./assets'));
+
+//use express layout
+app.use(expressLayouts);
+
+//set default layout
+app.set('layout', './layouts/layout');
+
+//extract styles and scripts from subpages to layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 //use express router
 app.use('/', require('./routes'));
+
 
 
 //set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+ 
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server :${err}`);
