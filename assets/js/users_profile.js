efc580656e59c9de5644b704cc5f5aba19290087
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2023-01-06 23:37:18 +0530

    change

--- assets/js/users_profile.js
+++ assets/js/users_profile.js
@@ -1,28 +1,40 @@
-{   
-    
-
-    let avatar_url = $('#user-avatar-img').attr('src');
+{
+  let avatar_url = $('#user-avatar-img').attr('src');
+  $('#user-details').show();
+  $('#edit-form').hide();
+  $('#edit-profile').click(function (e) {
+    e.preventDefault();
+    $('#user-details').hide();
+    $('#edit-form').show();
+  });
+  $('#cancel-button').click(function (e) {
+    e.preventDefault();
+    $('#user-avatar-img').attr('src', avatar_url);
     $('#user-details').show();
     $('#edit-form').hide();
-    $('#edit-profile').click(function (e) { 
-        e.preventDefault();
-        $('#user-details').hide();
-        $('#edit-form').show();
+  });
 
-    });
-    $('#cancel-button').click(function(e){
-        e.preventDefault();
-        $('#user-avatar-img').attr('src', avatar_url);
-        $('#user-details').show();
-        $('#edit-form').hide();
-    });
+  $('#avatar').change(function (e) {
+    const file = this.files[0];
 
-    $('#avatar').change(function (e) { 
-        
-        const file = this.files[0];
-        if(file){
-            $('#user-avatar-img').attr('src', URL.createObjectURL(file));
+    var fileName = $(this).val();
+    var idxDot = fileName.lastIndexOf('.') + 1;
+    var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+    if (extFile == 'jpg' || extFile == 'jpeg' || extFile == 'png') {
+      //TO DO
+      const fileSize = file.size / 1024 / 1024; // in MiB
+      if (fileSize > 2) {
+        alert('File size exceeds 2 MiB');
+        $(this).val('');
+      } else {
+        // Proceed further
+        if (file) {
+          $('#user-avatar-img').attr('src', URL.createObjectURL(file));
         }
-    });
+      }
+    } else {
+      alert('Only jpg/jpeg and png files are allowed!');
+      $(this).val('');
+    }
+  });
 }
-    
