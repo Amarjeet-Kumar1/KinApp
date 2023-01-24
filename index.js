773cbd44df6bd682f1ad2337c522b6481fbc8b70
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2023-01-25 03:19:19 +0530

    updated

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const env = require('./config/environment');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
 
 const session = require('express-session');
 const passport = require('passport');
@@ -15,23 +13,23 @@ const passportJWT = require('./config/possport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
 const MongoStore = require('connect-mongo');
- 
+
 const sassMiddleware = require('node-sass-middleware');
 
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
- 
-
 
 const path = require('path');
 
-app.use(sassMiddleware({
+app.use(
+  sassMiddleware({
     src: path.join(__dirname, env.asset_path, 'scss'),
     dest: path.join(__dirname, env.asset_path, 'css'),
     debug: true,
     outputStyle: 'expanded',
-    prefix: '/css' 
-}));
+    prefix: '/css',
+  })
+);
 
 app.use(express.urlencoded());
 
@@ -41,47 +39,46 @@ app.use(cookieParser());
 app.use(express.static(env.asset_path));
 //make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
- 
+
 //use express layout
 app.use(expressLayouts);
-  
+
 //set default layout
 app.set('layout', './layouts/layout');
 
 //extract styles and scripts from subpages to layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
- 
-  
-  
 
 //set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 //mongo store is used to strore the session cookie in the db
-app.use(session({
+app.use(
+  session({
     name: 'kinApp',
     //TODO change the secret before deployment in production mode
     secret: env.session_cookie_key,
     saveUninitialized: false, //don;t create session ultil something strord
     resave: false, //don't save session if unmodified
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
-    store: MongoStore.create({
+    store: MongoStore.create(
+      {
         mongoUrl: env.db_url,
-        autoRemove: 'disabled'
-    }, function(err){
+        autoRemove: 'disabled',
+      },
+      function (err) {
         console.log(err || 'connet-mongodb setup ok');
-    }
-    )
-
-}));
- 
+      }
+    ),
+  })
+);
 
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 //put after session is used, it uses session cookies
@@ -92,8 +89,4 @@ app.use(customMware.setFlash);
 //use express router after passport
 app.use('/', require('./routes'));
 
- 
- 
 module.exports = app;
-
-
--- models/user.js
+++ models/user.js
@@ -4,62 +4,67 @@ const path = require('path');
 
 const AVATAR_PATH = path.join('/uploads/users/avatars');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
     ],
     comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
     ],
-    avatar:{
-        type: String
+    avatar: {
+      type: String,
     },
     friendship: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Friendship'
-        }
-    ]
-    
-}, {
-    timestamps: true
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Friendship',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
 });
 
+//statics methods
 
-let storage = multer.diskStorage({ 
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '..', AVATAR_PATH));
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix);
-    }
-  });
-
-  //statics methods
-
-  userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
-  userSchema.statics.avatarPath = AVATAR_PATH;
-
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  'avatar'
+);
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
