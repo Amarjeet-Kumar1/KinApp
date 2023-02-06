4bbb5fbd59b7ebfb6f987f78f19a583e24bb3038
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2023-02-06 19:36:06 +0530

    ff

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
-const port = process.env.PORT || 8000; 
-const connectionParams={
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-}
+const port = process.env.PORT || 8000;
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(env.db_url,connectionParams)
-    .then( () => {
-        console.log('Connected to the database ');
-        const app = require('../index');
-        const httpserver = app.listen(port, function(err){
-            if(err){
-                console.log(`Error in running the server :${err}`);
-            }
-            
-            console.log(`Server is running on port: ${port}`);
-        });
-        const chatSockets = require('./chat_sockets').chatSockets(httpserver);
-    })
-    .catch( (err) => {
-        console.error(`Error connecting to the database. ${err}`);
+mongoose
+  .connect(env.db_url, connectionParams)
+  .then(() => {
+    console.log('Connected to the database ');
+    const app = require('../index');
+    const httpserver = app.listen(port, function (err) {
+      if (err) {
+        console.log(`Error in running the server :${err}`);
+      }
+
+      console.log(`Server is running on port: ${port}`);
     });
-
-
+    const chatSockets = require('./chat_sockets').chatSockets(httpserver);
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. ${err}`);
+  });
